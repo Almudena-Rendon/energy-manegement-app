cfb509d8bac9c106f2beae4745a2cfb1c7ccdbf1
Almudena Rendón Fernández <126793941+Almudena-Rendon@users.noreply.github.com>
2024-06-23 21:19:14 +0200

    style: add responsive classes

--- src/common/Calendar.jsx
+++ src/common/Calendar.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react"
-import moment from 'moment'
-import Datepicker from "tailwind-datepicker-react"
-import { ArrowLeft, ArrowRight } from "../utils/Index"
+import { useState } from "react";
+import moment from 'moment';
+import Datepicker from "tailwind-datepicker-react";
+import { ArrowLeft, ArrowRight } from "../utils/Index";
 
 const Calendar = ({ request, setRequest, dateType }) => {
 
@@ -28,7 +28,7 @@ const Calendar = ({ request, setRequest, dateType }) => {
 			prev: () => <span><ArrowLeft /></span>,
 			next: () => <span><ArrowRight /></span>,
 		},
-		datepickerClassNames: "top-12 ",
+		datepickerClassNames: "absolute mb-2 top-[-24.8rem] lg:top-9",
 		defaultDate: dateType === "startDate" ? new Date(new Date().getFullYear(), new Date().getMonth() - 1, new Date().getDate())  : new Date(),
 		language: "en",
 		disabledDates: [],
@@ -41,31 +41,31 @@ const Calendar = ({ request, setRequest, dateType }) => {
 			month: "long",
 			year: "numeric"
 		}
-	}
+	};
 	
-	const [show, setShow] = useState(false)
+	const [show, setShow] = useState(false);
 
 	const handleChange = (selectedDate) => {
-    let formattedDate
+		let formattedDate
 
-    if (dateType === "startDate") {
-      formattedDate = moment(selectedDate).startOf("day").format("YYYY-MM-DD[T]HH:mm")
-      setRequest({ ...request, startDate: formattedDate })
-    } else if (dateType === "endDate") {
-      formattedDate = moment(selectedDate).endOf("day").format("YYYY-MM-DD[T]HH:mm")
-      setRequest({ ...request, endDate: formattedDate })
-    }
-	}
+		if (dateType === "startDate") {
+			formattedDate = moment(selectedDate).startOf("day").format("YYYY-MM-DD[T]HH:mm");
+			setRequest({ ...request, startDate: formattedDate });
+		} else if (dateType === "endDate") {
+			formattedDate = moment(selectedDate).endOf("day").format("YYYY-MM-DD[T]HH:mm");
+			setRequest({ ...request, endDate: formattedDate });
+		}
+	};
 
-  const handleClose = (state) => {
-    setShow(state)
-  }
+	const handleClose = (state) => {
+		setShow(state);
+	};
 
 	return (
-		<div>
+		<div className="relative lg:static">
 			<Datepicker options={options} onChange={handleChange} show={show} setShow={handleClose} />
 		</div>
-	)
+	);
 }
 
-export default Calendar
+export default Calendar;
